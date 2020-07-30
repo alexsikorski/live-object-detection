00fe962031eb9fa1a61f89f23bb59f9e354af8d6
alexsikorski <alexsikorski22@gmail.com>
2020-07-30 23:50:51 +0100

    detect.py now draws boxes around located objects and displays name with percentage

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# knife-detect
-Algorithm that recognises knives with use of TensorFlow
+# Live Object Detection
+Detect objects in a webcam feed using OpenCV. 
+## What's being used
+- Python 3.8
+- OpenCV
+- [YOLO V3](https://pjreddie.com/darknet/yolo/) tiny/416 cfg and weights
+- Your CPU/GPU
+## How to run
+- Download respective [YOLO V3 cfg and weights ](https://pjreddie.com/darknet/yolo/) files, I used YOLOv3-416 and YOLOv3-tiny.
+- Configure as desired.
+- Run *detect.py*.
+## Configurations
+More than one camera?
+```python
+capture = cv2.VideoCapture(0) # where 0 is the first connected device
+```
+How do I display objects with lower confidence percentages?
+```python
+confidence_threshold = 0.8 # alter this value where 0.8 is 80%
+```
+How do I use my gpu?
+```python
+# uncomment these
+#network.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#network.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+```
+## Result
+![Detecting a mobile phone and a person](screenshots/cell%20phone.png)
--- detect.py
+++ detect.py
@@ -12,17 +12,23 @@ class_names = []
 with open(class_file, 'rt') as f:
     class_names = f.read().rstrip('\n').split('\n')
 
-
+# tiny weights (higher frame rate, lower accuracies)
 #model_config = 'yolov3-tiny.cfg'
 #model_weights = 'yolov3-tiny.weights'
 
+# 416 weights (lower frame rate, higher accuracies)
 model_config = 'yolov3.cfg'
 model_weights = 'yolov3.weights'
 
 network = cv2.dnn.readNetFromDarknet(model_config, model_weights)
 
+# use CPU
 network.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 network.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# use GPU (CUDA)
+#network.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#network.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 def find_objects(outputs, img):
     hT, wT, cT = img.shape
@@ -37,7 +43,7 @@ def find_objects(outputs, img):
             confidence = scores[class_id]
             if confidence > confidence_threshold:
                 width, height = int(detection[2] * wT), int(detection[3] * hT)
-                x, y = int(detection[0] * wT - width/2), int(detection[1] * hT - height/2)
+                x, y = int(detection[0] * wT - width / 2), int(detection[1] * hT - height / 2)
                 bounding_box.append([x, y, width, height])
                 class_ids.append(class_id)
                 confidence_values.append(float(confidence))
@@ -50,16 +56,17 @@ def find_objects(outputs, img):
         x, y, w, h = box[0], box[1], box[2], box[3]
         cv2.rectangle(img, (x, y), (x + w, y + h), (100, 100, 255), 2)
         cv2.putText(img, f'{class_names[class_ids[i]]} {int(confidence_values[i] * 100)}%',
-                    (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (100,100,255), 2)
+                    (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+
 
 while True:
     success, img = capture.read()
 
-    blob = cv2.dnn.blobFromImage(img, 1/255, (wh_target, wh_target), [0, 0, 0], 1, crop=False)
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (wh_target, wh_target), [0, 0, 0], 1, crop=False)
     network.setInput(blob)
 
     layer_names = network.getLayerNames()
-    output_names = [layer_names[i[0]-1] for i in network.getUnconnectedOutLayers()]
+    output_names = [layer_names[i[0] - 1] for i in network.getUnconnectedOutLayers()]
 
     outputs = network.forward(output_names)
 
